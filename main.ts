19948609b28ef9bff04615c763cf258da0eef48f
anitahondoziv-web <anita.hondoziv@icloud.com>
2026-04-27 10:53:06 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.showNumber(1, 1000)
+basic.showNumber(2, 2000)
+basic.showNumber(3, 3000)
+basic.forever(function () {
+    basic.showNumber(1, 1000)
+    basic.showNumber(2, 2000)
+    basic.showNumber(3, 3000)
+})
